6eced990c8696571662500d04b82d009bfda6bd5
john conroy <john@johns-MacBook-Pro.local>
2013-12-24 23:07:39 +0000

    updated views and urls

--- maisite/urls.py
+++ maisite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from mysite import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'maisite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^site/', include('mysite.urls')),
+    url(r'^admin/', include(admin.site.urls)),    
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+
+def index(request):
+    return HttpResponse("<h2>Hello, this is the home page</h2>")
+
+
+def about(request):
+    return HttpResponse("This is the About page")
